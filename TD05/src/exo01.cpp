af046158ff6a74f4a87d5d006fe2c193ebd001ee
angelegrd <angele.douet@edu.esiee.fr>
2024-06-07 21:50:11 +0200

    commit td05 début

--- TD05/src/exo01.cpp
+++ TD05/src/exo01.cpp
@@ -1,50 +1,40 @@
-#include <vector>
 #include <iostream>
-#include <cstdlib>
 #include <string>
-#include <stack>
 #include <algorithm>
+#include <vector>
 #include <numeric>
 
-size_t folding_string_hash(std::string const& s, size_t max){
-    size_t somme{0};
-    for (int i=0; i<s.size(); i++){
-        somme+=s[i];
-    }
-    return somme%max;
-};
-
-size_t folding_string_ordered_hash(std::string const& s, size_t max){
-    size_t somme{0};
-    for (int i=0; i<s.size(); i++){
-        somme+=s[i]*i;
-    }
-    return somme%max;
-};
-
-
-size_t polynomial_rolling_hash(const std::string& s, size_t p, size_t m){
-    size_t somme{0};
-    
-    for (int i{0}; i<s.size(); i++){
-        int power{i};
-        int p{0};
-        int result{1};
-        for(int j{1}; j<=power; j++){
-            result*=p;
-        }
-        somme+=s[i]*result;
-    }
-    return somme%m;
-
-};
-
-int main(){
-    std::cout<< folding_string_hash("cba", 200)<<std::endl;
-    std::cout<< folding_string_ordered_hash("cba", 200);
-
-    return 0;
+size_t folding_string_hash(std::string const &s, size_t max)
+{
+    return std::accumulate(s.begin(), s.end(), 0, [](int a, int b){ return a + b; }) % max;
 }
 
+size_t folding_string_ordered_hash(std::string const &s, size_t max)
+{
+    size_t hash{0};
+    for (size_t i{0}; i < s.size(); i++)
+        hash += s[i] * i;
+    return hash % max;
+}
 
+size_t polynomial_rolling_hash(const std::string &s, size_t p, size_t m)
+{
+    size_t hash{0};
+    size_t puissance{1};
+    for (size_t i{0}; i < s.size(); i++)
+    {
+        hash += s[i] * puissance;
+        puissance *= p;
+    }
+    return hash % m;
+}
 
+int main()
+{
+    size_t max{1024};
+    std::string phrase{"Bonjour michel"};
+    std::cout << folding_string_hash(phrase, max) << std::endl;
+    std::cout << folding_string_ordered_hash(phrase, max) << std::endl;
+    std::cout << polynomial_rolling_hash(phrase, 21, max) << std::endl;
+    return 0;
+}
