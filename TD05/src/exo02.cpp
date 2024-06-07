1747f59c414dec24e800897cb258aecc285fcbed
angelegrd <angele.douet@edu.esiee.fr>
2024-06-07 22:07:53 +0200

    suite td05 oups

--- TD05/src/exo02.cpp
+++ TD05/src/exo02.cpp
@@ -1,8 +1,30 @@
-#include <vector>
 #include <iostream>
-#include <cstdlib>
+#include <vector>
 #include <string>
-#include <stack>
-#include <algorithm>
-#include <numeric>
+#include <cstdlib>
 
+std::string random_name(size_t size) {
+    std::string name {""};
+    // Optimisation pour que la chaîne de caractère ne réalloue pas de la mémoire à chaque caractère ajouté
+    
+    name.reserve(size);
+    for(size_t i {0}; i < size; ++i) {
+        name.push_back('A' + std::rand() % 26);
+    }
+    return name;
+}
+
+std::vector<std::pair<std::string, float>> get_robots_fix(size_t size) {
+    std::vector<std::pair<std::string, float>> robots_fix {};
+    // Meme optimisation que dans random_name()
+    
+    robots_fix.reserve(size);
+    for (size_t i {0}; i < size; ++i) {
+        // random name 
+        std::string robotName { random_name(2) };
+        // random cost
+        float cost {static_cast<float>(std::rand()) / RAND_MAX * 1000.0f};
+        robots_fix.push_back(std::make_pair(robotName, cost));
+    }
+    return robots_fix;
+}
